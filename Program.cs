73b665a0b2ea5cf6632de78f877803cf23e69f2e
coder-dr-strange <askdrchitrangda@gmail.com>
2025-03-21 16:31:25 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OopsConcept
 {
@@ -12,7 +8,6 @@ namespace OopsConcept
         {
             string Name = "Chitrangda";
 
-            
             Console.WriteLine("Hi " + Name);
             Console.ReadLine();
         }
